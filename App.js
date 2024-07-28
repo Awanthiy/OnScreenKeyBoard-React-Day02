c5522b6c15aa0e05e60751077f2529f6ddc07e67
Awanthi Malawanage <121420903+Awanthiy@users.noreply.github.com>
2024-07-28 09:59:57 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+ 
+  
+ 
+import Keyboard from './components/Keyboard'; 
+  
+function App() { 
+  return ( 
+    <div className="App"> 
+      <Keyboard/> 
+    </div> 
+  ); 
+} 
+  
+export default App; 
